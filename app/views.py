9d03ebffeda3d43cfe01834852a3d994df1eaed8
taibon38 <38231054+taibon38@users.noreply.github.com>
2020-11-15 23:33:34 +0900

    メール登録機能の実装

--- app/views.py
+++ app/views.py
@@ -1,10 +1,30 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import CustomUserCreationForm
 from django.shortcuts import render
 
-# Create your views here.
+# メール認証機能用で追加
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import (
+    LoginView, LogoutView
+)
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.signing import BadSignature, SignatureExpired, loads, dumps
+from django.http import Http404, HttpResponseBadRequest
+from django.template.loader import render_to_string
+from django.views import generic
+from .forms import (
+    LoginForm, UserCreateForm
+)
+from django.core.mail import BadHeaderError #送信時のエラー解消目的
 
+User = get_user_model()
+
+
+# Create your views here.
 
 def index(request):
     return render(request, 'app/index.html')
@@ -24,3 +44,73 @@ def signup(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'app/signup.html', {'form': form})
+
+# メール認証時に追加
+class UserCreate(generic.CreateView):
+    """ユーザー仮登録"""
+    template_name = 'app/user_create.html'
+    form_class = UserCreateForm
+
+    def form_valid(self, form):
+        """仮登録と本登録用メールの発行."""
+        # 仮登録と本登録の切り替えは、is_active属性を使うと簡単です。
+        # 退会処理も、is_activeをFalseにするだけにしておくと捗ります。
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+
+        # アクティベーションURLの送付
+        current_site = get_current_site(self.request)
+        domain = current_site.domain
+        context = {
+            'protocol': self.request.scheme,
+            'domain': domain,
+            'token': dumps(user.pk),
+            'user': user,
+        }
+
+        subject = render_to_string('app/mail_template/create/subject.txt', context).strip() #.strip()で改行を取り除く
+        message = render_to_string('app/mail_template/create/message.txt', context).strip()
+
+        user.email_user(subject, message) #宛先１名に対する送信メソッド。user.email_user('メールの件名', 'メールの本文')
+        return redirect('app:user_create_done')
+
+
+class UserCreateDone(generic.TemplateView):
+    """ユーザー仮登録したよ"""
+    template_name = 'app/user_create_done.html'
+
+
+class UserCreateComplete(generic.TemplateView):
+    """メール内URLアクセス後のユーザー本登録"""
+    template_name = 'app/user_create_complete.html'
+    timeout_seconds = getattr(settings, 'ACTIVATION_TIMEOUT_SECONDS', 60*60*24)  # デフォルトでは1日以内
+
+    def get(self, request, **kwargs):
+        """tokenが正しければ本登録."""
+        token = kwargs.get('token')
+        try:
+            user_pk = loads(token, max_age=self.timeout_seconds)
+
+        # 期限切れ
+        except SignaturxeExpired:
+            return HttpResponseBadRequest()
+
+        # tokenが間違っている
+        except BadSignature:
+            return HttpResponseBadRequest()
+
+        # tokenは問題なし
+        else:
+            try:
+                user = User.objects.get(pk=user_pk)
+            except User.DoesNotExist:
+                return HttpResponseBadRequest()
+            else:
+                if not user.is_active:
+                    # 問題なければ本登録とする
+                    user.is_active = True
+                    user.save()
+                    return super().get(request, **kwargs)
+
+        return HttpResponseBadRequest()
