2fae15fced44f7bae46e9fe7db790a67f131fa92
taibon38 <38231054+taibon38@users.noreply.github.com>
2020-12-11 22:36:43 +0900

    500エラーの詳細確認

--- marble/settings.py
+++ marble/settings.py
@@ -223,3 +223,44 @@ MEDIA_URL = '/media/'
 if not DEBUG:
     import django_heroku
     django_heroku.settings(locals()) # django_herokuのsettings関数を実行している
+
+
+# 500エラーの詳細表示
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
