986e8144b3585f566b9fb643c48e041c8ccb32db
taibon38 <38231054+taibon38@users.noreply.github.com>
2020-12-14 09:08:53 +0900

    環境変数をenv()の書き方に変更

--- marble/settings.py
+++ marble/settings.py
@@ -230,9 +230,9 @@ if not DEBUG:
     EMAIL_PORT = 587
 
     # S3導入時に追加
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
