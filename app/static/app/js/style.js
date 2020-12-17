330389437957c850320a73ec2054d90bc85e34f0
taibon38 <38231054+taibon38@users.noreply.github.com>
2020-12-17 19:27:56 +0900

    別ページからのスクロール追加

--- app/static/app/js/style.js
+++ app/static/app/js/style.js
@@ -20,19 +20,35 @@
 // }); 
 
 // スムーズスクロール部分の記述
-    $(function(){
-    // #で始まるアンカーをクリックした場合に処理
-    $('a[href^="#"]').click(function() {
-        // スクロールの速度
-        var speed = 400; // ミリ秒
-        // アンカーの値取得
-        var href= $(this).attr("href");
-        // 移動先を取得
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        // 移動先を数値で取得
-        var position = target.offset().top;
-        // スムーススクロール
-        $('body,html').animate({scrollTop:position}, speed, 'swing');
-        return false;
-    });
-    });
+$(function() {
+    var headerHeight = $('.sticky-top').outerHeight();
+    var urlHash = location.hash;
+    if(urlHash) {
+        $('body,html').stop().scrollTop(0);
+        setTimeout(function(){
+            var target = $(urlHash);
+            var position = target.offset().top - headerHeight;
+            $('body,html').stop().animate({scrollTop:position}, 500);
+        }, 100);
+    }
+ 
+  // ①aタグをクリックし、href属性に # から始まるリンクが設定してあったら発動
+  $('a[href^="/#"]').click(function(){
+ 
+    // ②クリックしたaタグのhref属性（リンク先URI）を取得し、変数に格納
+    var href = $(this).attr('href');
+ 
+    // ③上で取得した値が#か空白だったら'html'を、それ以外だったら先ほど取得したhref属性の値を変数に格納
+    var target = $(href == '#' || href === '' ? 'html' : href);
+ 
+    // ④変数targetのページトップからの位置を取得し、変数に格納
+    var position = target.offset().top;
+ 
+    // ⑤scrollTopに上で取得した位置を設定し、ヌルヌルとスクロールさせる
+    $('html,body').animate({scrollTop : position}, 500);
+ 
+    // ⑥a要素のデフォルトの機能を無効化する
+    return false;
+ 
+  });
+});
