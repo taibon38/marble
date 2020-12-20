a4dac2dbf3fe3c0c307e28f6ba3d89a0d4461a93
taibon38 <38231054+taibon38@users.noreply.github.com>
2020-12-20 20:45:38 +0900

    jquery修正

--- app/static/app/js/style.js
+++ app/static/app/js/style.js
@@ -10,7 +10,7 @@
 //     $(".accordion-wrap .accordion-text").not($(this).children().eq(1)).slideUp(300);
 // });
 
-// $(function(){
+// $(function(){w
 //     $('.js-menu__item__link').each(function(){
 //         $(this).on('click',function(){
 //             $("+.submenu",this).slideToggle();
@@ -31,13 +31,11 @@ $(function() {
             $('body,html').stop().animate({scrollTop:position}, 500);
         }, 100);
     }
-    console.log('bbb')
   // ①aタグをクリックし、href属性に # から始まるリンクが設定してあったら発動
   $('a[href^="/#"]').click(function(){
  
     // ②クリックしたaタグのhref属性（リンク先URI）を取得し、変数に格納
     var href = $(this).attr('href');
-    console.log(href)
 
     // ③上で取得した値が#か空白だったら'html'を、それ以外だったら先ほど取得したhref属性の値を変数に格納
     var target = $(href == '#' || href === '' ? 'html' : href.slice(1));
