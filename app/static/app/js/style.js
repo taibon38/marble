5a2d921e6f1c4081211196b41666f12ea0d9ad0d
taibon38 <38231054+taibon38@users.noreply.github.com>
2020-12-23 08:03:44 +0900

    アコーディオンスタイル変更(bootstrap5追加

--- app/static/app/js/style.js
+++ app/static/app/js/style.js
@@ -1,23 +1,3 @@
-// /* acordion */
-// $(".accordion-wrap").on("click", function(){   
-//     $(this).children().eq(1).slideToggle(300);  
-//     $(this).children().eq(0).toggleClass("accordion-no-bar");
-//     $(this).siblings().find(".accordion-header").removeClass("accordion-gold");
-//     $(this).siblings().find(".accordion-header i").removeClass("rotate-fa");
-//     $(this).find(".accordion-header").toggleClass("accordion-gold");
-//     $(this).find(".fa").toggleClass("rotate-fa");
-
-//     $(".accordion-wrap .accordion-text").not($(this).children().eq(1)).slideUp(300);
-// });
-
-// $(function(){w
-//     $('.js-menu__item__link').each(function(){
-//         $(this).on('click',function(){
-//             $("+.submenu",this).slideToggle();
-//             return false;
-//         });
-//     });
-// }); 
 
 // スムーズスクロール部分の記述
 $(function() {
